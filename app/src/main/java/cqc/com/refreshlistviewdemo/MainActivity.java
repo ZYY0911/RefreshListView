0a71737da3ec194bfa4f446b25860edbfa32f62d
北方的天空2008 <cui1168805219@163.com>
2018-03-22 08:59:44 +0800

    上拉加载

--- app/src/main/java/cqc/com/refreshlistviewdemo/MainActivity.java
+++ app/src/main/java/cqc/com/refreshlistviewdemo/MainActivity.java
@@ -1,25 +1,57 @@
 package cqc.com.refreshlistviewdemo;
 
+import android.content.Context;
+import android.os.Handler;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.ArrayAdapter;
 import android.widget.TextView;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
     private TextView tv;
     private RefreshListView listView;
 
+    private Context context;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        context = this;
         tv = findViewById(R.id.tv);
         tv.setPadding(0, -50, 0, 0);
 
         listView = findViewById(R.id.listView);
         ArrayAdapter<CharSequence> adapter = ArrayAdapter.createFromResource(this, R.array.province, android.R.layout.simple_list_item_1);
         listView.setAdapter(adapter);
+
+        listView.setOnRefreshListener(new RefreshListView.OnRefreshListener() {
+            @Override
+            public void onRefresh() {
+                new Handler().postDelayed(new Runnable() {
+                    @Override
+                    public void run() {
+                        ToastUtil.showShortToast(context, "刷新成功");
+                        listView.finishRefresh();
+                    }
+                }, 2000);
+            }
+        });
+
+        listView.setOnLoadMoreListener(new RefreshListView.OnLoadMoreListener() {
+            @Override
+            public void onLoadMore() {
+                new Handler().postDelayed(new Runnable() {
+                    @Override
+                    public void run() {
+                        ToastUtil.showShortToast(context, "上拉加载成功");
+                        listView.finishLoadMore();
+                    }
+                }, 2000);
+            }
+        });
     }
 }
--- app/src/main/java/cqc/com/refreshlistviewdemo/RefreshListView.java
+++ app/src/main/java/cqc/com/refreshlistviewdemo/RefreshListView.java
@@ -1,12 +1,16 @@
 package cqc.com.refreshlistviewdemo;
 
 import android.content.Context;
+import android.support.v4.widget.SwipeRefreshLayout;
 import android.util.AttributeSet;
 import android.view.MotionEvent;
 import android.view.View;
+import android.view.animation.AnimationUtils;
+import android.widget.AbsListView;
 import android.widget.FrameLayout;
 import android.widget.ImageView;
 import android.widget.ListView;
+import android.widget.ProgressBar;
 import android.widget.TextView;
 
 /**
@@ -18,7 +22,7 @@ import android.widget.TextView;
  * <p>
  * 下拉刷新中：
  * move中判断是 下拉刷新 还是 释放刷新
- * up中进行刷新操作
+ * up中进行刷新操作:  当前是下拉刷新，则隐藏header;当前是释放刷新，则进行刷新动作。
  * <p>
  * <p>
  * <p>
@@ -27,7 +31,7 @@ import android.widget.TextView;
  * 刷新你中：headerView完全显示出来，手指离开屏幕 ACTION_UP
  */
 
-public class RefreshListView extends ListView {
+public class RefreshListView extends ListView implements AbsListView.OnScrollListener {
 
     //header
     private View headerView;
@@ -35,6 +39,11 @@ public class RefreshListView extends ListView {
     private ImageView ivArrowHeader;
     private TextView tvStateHeader;
     private int headerViewHeight;
+    private ProgressBar progressBarHeader;
+
+    //footer
+    private View footerView;
+    private int footerViewHeight;
 
 
     private int downY;
@@ -44,7 +53,6 @@ public class RefreshListView extends ListView {
     public final int DOWN_REFRESH = 1;
     public final int RELEASE_REFRESH = 2;
     public final int REFRESHING = 3;
-
     private int currentState = DOWN_REFRESH;//当前状态是下拉刷新
 
 
@@ -61,14 +69,16 @@ public class RefreshListView extends ListView {
         LogUtil.d("RefreshListView");
         initHeaderView();
         initFooterView();
+        setOnScrollListener(this);
     }
 
 
     private void initHeaderView() {
         headerView = View.inflate(getContext(), R.layout.header_view, null);
-        flHeader = findViewById(R.id.fl_header);
-        ivArrowHeader = findViewById(R.id.iv_arrow_header);
-        tvStateHeader = findViewById(R.id.tv_state_header);
+        flHeader = headerView.findViewById(R.id.fl_header);
+        progressBarHeader = headerView.findViewById(R.id.pb_header);
+        ivArrowHeader = headerView.findViewById(R.id.iv_arrow_header);
+        tvStateHeader = headerView.findViewById(R.id.tv_state_header);
 
         headerView.measure(0, 0);
         headerViewHeight = headerView.getMeasuredHeight();
@@ -77,7 +87,12 @@ public class RefreshListView extends ListView {
     }
 
     private void initFooterView() {
+        footerView = View.inflate(getContext(), R.layout.footer_view, null);
+        footerView.measure(0, 0);
+        footerViewHeight = footerView.getMeasuredHeight();
+        footerView.setPadding(0, -footerViewHeight, 0, 0);
 
+        addFooterView(footerView);
     }
 
 
@@ -91,15 +106,113 @@ public class RefreshListView extends ListView {
                 moveY = (int) ev.getY();
                 int instance = moveY - downY;
                 int paddingTop = -headerViewHeight + instance;
+
                 if (instance > 0 && getFirstVisiblePosition() == 0 && currentState != REFRESHING) {
-                    headerView.setPadding(0, paddingTop, 0, 0);
+                    if (paddingTop < 0 && currentState != DOWN_REFRESH) {
+                        currentState = DOWN_REFRESH;
+                        //下拉刷新
+                        tvStateHeader.setText("下拉刷新");
+                        ivArrowHeader.startAnimation(AnimationUtil.downAnim());
+                    } else if (paddingTop > 0 && currentState != RELEASE_REFRESH) {
+                        currentState = RELEASE_REFRESH;
+                        //释放刷新
+                        tvStateHeader.setText("释放刷新");
+                        ivArrowHeader.startAnimation(AnimationUtil.UpAnim());
+                    }
+                    int maxTop = getMeasuredHeight() / 2;
+                    if (paddingTop > maxTop) {
+                        headerView.setPadding(0, maxTop, 0, 0);
+                    } else {
+                        headerView.setPadding(0, paddingTop, 0, 0);
+                    }
+                    return true;
                 }
                 break;
             case MotionEvent.ACTION_UP:
-
+                if (currentState == DOWN_REFRESH) {
+                    headerView.setPadding(0, -headerViewHeight, 0, 0);
+                } else if (currentState == RELEASE_REFRESH) {
+                    currentState = REFRESHING;
+                    headerView.setPadding(0, 0, 0, 0);
+                    //执行刷新操作
+                    tvStateHeader.setText("刷新中...");
+                    ivArrowHeader.setVisibility(View.GONE);
+                    ivArrowHeader.clearAnimation();
+                    progressBarHeader.setVisibility(View.VISIBLE);
+                    if (onRefreshListener != null) {
+                        onRefreshListener.onRefresh();
+                    }
+                }
                 break;
         }
-
         return super.onTouchEvent(ev);
     }
+
+
+    /**
+     * 结束刷新
+     */
+    public void finishRefresh() {
+        headerView.setPadding(0, -headerViewHeight, 0, 0);
+        currentState = DOWN_REFRESH;
+        ivArrowHeader.setVisibility(View.VISIBLE);
+        ivArrowHeader.clearAnimation();
+        progressBarHeader.setVisibility(View.GONE);
+    }
+
+
+    /**
+     * 下拉刷新回调
+     *
+     * @param onRefreshListener
+     */
+    public void setOnRefreshListener(OnRefreshListener onRefreshListener) {
+        this.onRefreshListener = onRefreshListener;
+    }
+
+    public interface OnRefreshListener {
+        void onRefresh();
+    }
+
+    private OnRefreshListener onRefreshListener;
+
+
+    //  setOnScrollListener
+    @Override
+    public void onScrollStateChanged(AbsListView view, int scrollState) {
+        if (scrollState == SCROLL_STATE_FLING || scrollState == SCROLL_STATE_IDLE) {
+            if (getLastVisiblePosition() == getCount() - 1) {
+                footerView.setPadding(0, 0, 0, 0);
+//                smoothScrollToPosition(getCount());
+                setSelection(getCount());
+                if (onLoadMoreListener != null) {
+                    onLoadMoreListener.onLoadMore();
+                }
+            }
+        }
+    }
+
+    @Override
+    public void onScroll(AbsListView view, int firstVisibleItem, int visibleItemCount, int totalItemCount) {
+
+    }
+
+
+    //加载更多回调
+    public interface OnLoadMoreListener {
+        void onLoadMore();
+    }
+
+    private OnLoadMoreListener onLoadMoreListener;
+
+    public void setOnLoadMoreListener(OnLoadMoreListener onLoadMoreListener) {
+        this.onLoadMoreListener = onLoadMoreListener;
+    }
+
+
+    //结束加载更多
+    public void finishLoadMore() {
+        footerView.setPadding(0, -footerViewHeight, 0, 0);
+    }
+
 }
